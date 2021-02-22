b7528a34e7647fa56b31ceeeb8f7b9c9e1a898cb
Luis Perez <luisperezgarcia707@gmail.com>
2021-02-22 07:08:02 -0500

    Translate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
+/* eslint-disable import/no-dynamic-require */
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { TranslatorProvider } from 'react-translate';
 
 import Home from './pages/Home';
+
+import { LangContext } from './components/Context';
+
 import './App.css';
 
 function App() {
+  const [lang, setLang] = useState(localStorage.getItem('Lang') || 'en');
+
+  useEffect(() => {
+    if (localStorage.getItem('Lang') === null) {
+      localStorage.setItem('Lang', 'en');
+    }
+  }, []);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <LangContext.Provider value={{ lang, setLang }}>
+        <TranslatorProvider
+          // eslint-disable-next-line global-require
+          translations={require(`./assets/lang/${lang}.json`)}
+        >
+          <BrowserRouter>
+            <Switch>
+              <Route path="/">
+                <Home name="Home" />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </TranslatorProvider>
+      </LangContext.Provider>
     </div>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,9 @@
-function Home() {
-  return <div>Hola</div>;
+/**
+ * @param {Object} props - properties
+ * @param {String} props.name - name
+ */
+function Home({ name }) {
+  return <div>{name}</div>;
 }
 
 export default Home;
